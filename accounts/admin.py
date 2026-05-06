56107714f6285029fba413efcf96734b6556c715
Muhammad Shamil <muhammadshamil.2402@gmail.com>
2026-05-06 15:29:48 +0530

    Initial static website upload

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AdminUser
-
-
-@admin.register(AdminUser)
-class AdminUserAdmin(admin.ModelAdmin):
-    list_display = ('email',)
-    search_fields = ('email',)
--- static/pwa/admin-sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-    console.log('Admin Service Worker Installed');
-});
-
-self.addEventListener('fetch', function(event) {
-    // Basic service worker (no caching yet)
-});
